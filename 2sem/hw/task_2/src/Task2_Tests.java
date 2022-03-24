f395d94c1e08b7ad97cfa2152bcbfc7e28ed8c48
FixSad <georgiy-ivanov-03@bk.ru>
2022-03-25 01:05:00 +0300

    Добавил тест bubbleSort_CreateMyBubbleSort_TryToSortLongerArrayWithDifferentNumbers

--- 2sem/hw/task_2/src/Task2_Tests.java
+++ 2sem/hw/task_2/src/Task2_Tests.java
@@ -26,10 +26,17 @@ public class Task2_Tests extends Assert {
 
     @Test
     public void bubbleSort_CreateMyBubbleSort_TryToSortDifferentNumbers(){
-
         BubbleSort<Integer> sorter = new BubbleSort<>();
         Integer[] myArray = {2, 1, 3};
         sorter.sort(myArray, new SomeComparator());
         assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
+    }
+
+    @Test
+    public void bubbleSort_CreateMyBubbleSort_TryToSortLongerArrayWithDifferentNumbers(){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+        Integer[] myArray = {2, 1, 3, 5, 7};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3, 5, 7}, myArray);
     }
 }
