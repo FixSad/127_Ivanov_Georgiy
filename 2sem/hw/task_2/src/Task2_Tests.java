97ec7a01df4556f264d73800e5346ba8b6d61f61
FixSad <georgiy-ivanov-03@bk.ru>
2022-03-25 01:45:31 +0300

    Добавил тест insertionSort_CreateMyInsertionSort_TryToSortDifferentNumbers

--- 2sem/hw/task_2/src/Task2_Tests.java
+++ 2sem/hw/task_2/src/Task2_Tests.java
@@ -72,4 +72,12 @@ public class Task2_Tests extends Assert {
         assertArrayEquals(new Integer[]{1}, myArray);
     }
 
+    @Test
+    public void insertionSort_CreateMyInsertionSort_TryToSortDifferentNumbers(){
+        InsertionSort<Integer> sorter = new InsertionSort<>();
+        Integer[] myArray = {1, 5, 2, 6};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 5, 6}, myArray);
+    }
+
 }
