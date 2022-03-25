4cfa1deeea0dd62ea5b94501d2ef7df740969ce8
FixSad <georgiy-ivanov-03@bk.ru>
2022-03-25 23:51:44 +0300

    Добавил тест mergeSort_CreateNewMergeSort_TryToCompareArrays

--- 2sem/hw/task_3/src/Task3_Tests.java
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -112,4 +112,18 @@ public class Task3_Tests extends Assert{
         sorter.sort(array, comparator);
         assertArrayEquals(new Integer[]{2, 2, 3, 4}, array);
     }
+
+    @Test
+    public void mergeSort_CreateNewMergeSort_TryToCompareArrays(){
+        MergeSort<Integer> sorter = new MergeSort<>();
+        Integer[] array = {1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1}, array);
+    }
 }
