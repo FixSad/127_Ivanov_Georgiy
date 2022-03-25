15473885d6e4369b5f3c51b698d16d14d36abfc7
FixSad <georgiy-ivanov-03@bk.ru>
2022-03-25 14:09:48 +0300

    Добавил тест selectionSort_CreateMySelectionSort_TryToSortLargeArray

--- 2sem/hw/task_2/src/Task2_Tests.java
+++ 2sem/hw/task_2/src/Task2_Tests.java
@@ -104,4 +104,12 @@ public class Task2_Tests extends Assert {
         assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
     }
 
+    @Test
+    public void selectionSort_CreateMySelectionSort_TryToSortLargeArray(){
+        SelectionSort<Integer> sorter = new SelectionSort<>();
+        Integer[] myArray = {1, 3, 2, 10, 5, 7, 4};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 7, 10}, myArray);
+    }
+
 }
