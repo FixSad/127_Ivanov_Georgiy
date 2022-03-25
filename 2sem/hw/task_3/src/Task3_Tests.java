58b7f98dfbfe70a9e9f8c7b6ce509590b15edcb5
FixSad <georgiy-ivanov-03@bk.ru>
2022-03-25 23:15:13 +0300

    Добавил новый тест quickSort_CreateNewQuickSort_TryToCompareArrays

--- 2sem/hw/task_3/src/Task3_Tests.java
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -15,4 +15,17 @@ public class Task3_Tests extends Assert{
         assertArrayEquals(new Integer[]{1}, array);
     }
 
+    @Test
+    public void quickSort_CreateNewQuickSort_TryToCompareArrays(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1}, array);
+    }
 }
