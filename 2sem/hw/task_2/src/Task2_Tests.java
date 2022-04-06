eb70b8de16f084757354802fa2cf761ed6e64026
FixSad <georgiy-ivanov-03@bk.ru>
2022-04-06 17:11:36 +0300

    Исправил ошибку

--- 2sem/hw/task_2/src/Task2_Tests.java
+++ 2sem/hw/task_2/src/Task2_Tests.java
@@ -4,16 +4,8 @@ public class Task2_Tests extends Assert {
     @Test
     public void bubbleSort_CreateMyBubbleSort_TryToSort (){
         BubbleSort<Integer> sorter = new BubbleSort<>();
-
-        MyComparator comparator = new MyComparator() {
-            @Override
-            public int compare(Object first, Object second) {
-                return 0;
-            }
-        };
-
         Integer[] myArray = {1,2,3};
-        sorter.sort(myArray, comparator);
+        sorter.sort(myArray, new SomeComparator());
         assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
     }
 
